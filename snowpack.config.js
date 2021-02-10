9fee63566d41e3c8382205f00b55623f68e5f106
Shane Jordan <jordan.shane@gmail.com>
2021-02-10 12:11:20 -0500

    add prettier, start rollup for builds

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,15 +1,29 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    static: '/',
-    src: '/dist',
+    static: "/",
+    src: "/dist",
   },
   plugins: [
-    '@snowpack/plugin-svelte'
+    "@snowpack/plugin-svelte",
+    // "snowpack-plugin-rollup-bundle",
+    // {
+    //   // equivalent to inputOptions.input from Rollup
+    //   entrypoints: "build/index.js",
+    //   extendConfig: (config) => {
+    //     // https://rollupjs.org/guide/en/#outputoptions-object
+    //     // config.outputOptions = { ... }
+
+    //     // https://rollupjs.org/guide/en/#inputoptions-object
+    //     // config.inputOptions = { ... }
+
+    //     return config;
+    //   },
+    // },
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    {"match": "routes", "src": ".*", "dest": "index.html"},
+    { match: "routes", src: ".*", dest: "index.html" },
   ],
   optimize: {
     /* Example: Bundle your final build: */
--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,21 @@
 <!-- App.svelte -->
 <script>
-    /* component logic will go here */
-  </script>
-  <style>
-    /* css will go here */
-  </style>
-  <div class="App">
-    <header class="App-header">
-      <a
-        class="App-link"
-        href="https://svelte.dev"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Boom starter kit!
-      </a>
-    </header>
-  </div>
+  /* component logic will go here */
+</script>
+
+<div class="App">
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Boom starter kit!
+    </a>
+  </header>
+</div>
+
+<style>
+  /* css will go here */
+</style>
