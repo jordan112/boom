6c8f5787728a6e8297ab8e06d832cfaaad515f04
Shane Jordan <jordan.shane@gmail.com>
2021-02-10 11:44:00 -0500

    add svelte

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    static: '/',
+    src: '/dist',
   },
   plugins: [
     '@snowpack/plugin-svelte'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
